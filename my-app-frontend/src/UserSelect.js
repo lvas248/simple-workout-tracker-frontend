cc40f3bc023b7e7fc085ecfd166351da9518ec12
Luis <lvas248@gmail.com>
2022-10-28 16:09:19 -0400

    create user dropdown menu for UserSelect component

--- my-app-frontend/src/UserSelect.js
+++ my-app-frontend/src/UserSelect.js
@@ -1,11 +1,26 @@
 import AddUser from "./AddUser"
 import UserEdit from "./UserEdit"
 
-function UserSelect(){
+import { useState } from 'react'
+
+function UserSelect({users}){
+
+
+    
+    const renderOptions = users.map( user => {
+        return <option key={user.id} value={user.id}>{user.user_name}</option>
+    })
 
     return (
         <div>
-            UserSelect
+            <h2>Select User</h2>
+            <div>
+                <label>Select User: </label>
+                <select>
+                    <option>Select</option>
+                    {renderOptions}
+                </select>
+            </div>
         </div>
     )
 }
