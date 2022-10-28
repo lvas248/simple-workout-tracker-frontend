faa843a832accec446d58db3510e40c9acbde3be
Luis <lvas248@gmail.com>
2022-10-28 15:49:07 -0400

    set up state and useEffect for initial Get request in App component

--- my-app-frontend/src/App.js
+++ my-app-frontend/src/App.js
@@ -1,7 +1,33 @@
 import './App.css';
+import { useEffect, useState } from 'react'
 
 
 function App() {
+
+  const URL = 'http://localhost:9292/'
+
+  const [ users, setUsers ] = useState([])
+  const [ exercises, setExercises ] = useState([])
+  const [ workouts, setWorkouts ] = useState([])
+
+  useEffect(()=>{
+    fetch(URL+'/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
+
+  useEffect(()=>{
+    fetch(URL+'/exercises')
+    .then(res => res.json())
+    .then(data => setExercises(data))
+  },[])
+
+  useEffect(()=>{
+    fetch(URL+'/workouts')
+    .then(res => res.json())
+    .then(data => setWorkouts(data))
+  },[])
+
   return (
     <div className="App">
       Simple Workout Tracker
