23f8a40497f241fa8a02b9ffaed5c4d926419aa1
Luis <lvas248@gmail.com>
2022-10-31 14:45:16 -0400

    setup form layout in WorkoutFrom component

--- my-app-frontend/src/WorkoutForm.js
+++ my-app-frontend/src/WorkoutForm.js
@@ -5,6 +5,45 @@ function WorkoutForm(){
     return (
         <form>
 
+            <div>
+
+                <h2>Track Your Set</h2>
+
+                <div>
+                    <label>Select Exercise: </label>
+                </div>
+
+                <div>
+                    <select>
+                        <option>Select</option>
+                    </select>
+                </div>
+
+                <label>Set Number:</label>
+
+                <div>
+                    <button type='button'>➖</button>
+                    <h4>0</h4>
+                    <button type='button'>➕</button>
+                </div>
+
+                <label>Weight: </label>
+
+                <div>
+                    <input></input>
+                </div>
+
+                <label>Reps: </label>
+
+                <div>
+                    <button type='button'>➖</button>
+                    <input />
+                    <button type='button'>➕</button>
+                </div>   
+
+            </div>
+
+            <button>Submit</button>
         </form>
     )
 }
