484318604fab127526a0d8518a8a64a5f682976e
Luis <lvas248@gmail.com>
2022-10-31 22:37:51 -0400

    filter workouts list to render only todays workouts

--- my-app-frontend/src/LogWorkout.js
+++ my-app-frontend/src/LogWorkout.js
@@ -3,11 +3,18 @@ import WorkoutTable from "./WorkoutTable"
 
 function LogWorkout({URL, workouts, currentUser, renderExerciseOptions, addToWorkoutList, deleteWorkoutFromList, updateWorkoutOnList}){
 
+    const today = new Date()
+    const todayFormatted = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`
+
+    const todaysWorkouts = workouts.filter( wrk => {
+        return wrk.created_at.includes(todayFormatted)
+    })
 
     return(
         <div>
-            <WorkoutForm URL={URL} currentUser={currentUser} renderExerciseOptions={renderExerciseOptions} addToWorkoutList={addToWorkoutList}/>
-            <WorkoutTable URL={URL} renderExerciseOptions={renderExerciseOptions} workouts={workouts} deleteWorkoutFromList={deleteWorkoutFromList} updateWorkoutOnList={updateWorkoutOnList}/>
+            <WorkoutForm URL={URL} currentUser={currentUser} renderExerciseOptions={renderExerciseOptions} addToWorkoutList={addToWorkoutList}/>            
+            <h2>Today's Workout</h2>
+            <WorkoutTable URL={URL} currentUser={currentUser} renderExerciseOptions={renderExerciseOptions} workouts={todaysWorkouts} deleteWorkoutFromList={deleteWorkoutFromList} updateWorkoutOnList={updateWorkoutOnList}/>
         </div>
     ) 
 }
