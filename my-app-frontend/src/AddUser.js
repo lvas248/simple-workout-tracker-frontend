a3dc8f6defcbd68f15adbdc0d28d1ab7ee882bf9
Luis <lvas248@gmail.com>
2022-11-01 20:29:22 -0400

    correct input width issue in AddUser component

--- my-app-frontend/src/AddUser.js
+++ my-app-frontend/src/AddUser.js
@@ -8,23 +8,28 @@ function AddUser({URL, addNewUserToUserList, clickAddBtn}){
 //Functions
     function handleSubmit(e){
         e.preventDefault()
-        fetch(URL+'users',{
-            method: 'POST',
-            headers:{
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                'user_name': newUser
+        if(newUser.length > 0){
+            fetch(URL+'users',{
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'user_name': newUser
+                })
             })
-        })
-        .then(res => res.json())
-        .then(data => addNewUserToUserList(data))
-        setNewUser('')
-        clickAddBtn()
+            .then(res => res.json())
+            .then(data => addNewUserToUserList(data))
+            setNewUser('')
+            clickAddBtn()
+        }
+        else alert('Text field cannot be left blank')
+     
     }
-    
+
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} id='addUserForm'>
+            <button type='button' onClick={clickAddBtn}>x</button>
             <input value={newUser} onChange={e => setNewUser(e.target.value)} placeholder='New username...'/>
             <button>Add</button>
         </form>
